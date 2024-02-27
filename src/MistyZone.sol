931fd4811744245dd019ebdb7857bc499bee6f61
jschiarizzi <9449596+cupOJoseph@users.noreply.github.com>
2024-02-27 12:08:15 -0700

    add zone

--- src/MistyZone.sol
+++ src/MistyZone.sol
@@ -1,4 +1,28 @@
 pragma solidity ^0.8.18;
 
+import "seaport/contracts/interfaces/ZoneInterface.sol";
 
+interface IPrivacyPool{
+
+    function belongsToPrivacyPool(address _user) external returns (bool);
+}
+
+contract MistyZone {
+
+    IPrivacyPool public privacyPool;
+
+    constructor(address _privacyPoolAddress){
+        privacyPool = IPrivacyPool(_privacyPoolAddress);
+    }
+
+    function validateOrder(
+        ZoneParameters calldata zoneParameters
+    ) external returns (bytes4 validOrderMagicValue){
+        //    address fulfiller;
+        //    address offerer;
+        require(privacyPool.belongsToPrivacyPool(zoneParameters.fulfiller));
+        require(privacyPool.belongsToPrivacyPool(zoneParameters.offerer));
+
+    }
+}
 
