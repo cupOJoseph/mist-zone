0a350f7cf2616d4b02759127d55193a6403a16d4
jschiarizzi <9449596+cupOJoseph@users.noreply.github.com>
2024-02-27 12:14:21 -0700

    add 165 override

--- src/MistyZone.sol
+++ src/MistyZone.sol
@@ -1,13 +1,14 @@
 pragma solidity ^0.8.18;
 
 import "seaport/contracts/interfaces/ZoneInterface.sol";
+import { ERC165 } from "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 
 interface IPrivacyPool{
 
     function belongsToPrivacyPool(address _user) external returns (bool);
 }
 
-contract MistyZone {
+contract MistyZone is ERC165, ZoneInterface{
 
     IPrivacyPool public privacyPool;
 
@@ -23,6 +24,35 @@ contract MistyZone {
         require(privacyPool.belongsToPrivacyPool(zoneParameters.fulfiller));
         require(privacyPool.belongsToPrivacyPool(zoneParameters.offerer));
 
+        // Return the selector of isValidOrder as the magic value.
+        validOrderMagicValue = ZoneInterface.validateOrder.selector; 
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC165, ZoneInterface) returns (bool) {
+        return
+            interfaceId == type(ZoneInterface).interfaceId ||
+            super.supportsInterface(interfaceId);
+    }
+
+        /**
+     * @dev Returns the metadata for this zone.
+     */
+    function getSeaportMetadata()
+        external
+        pure
+        override
+        returns (
+            string memory name,
+            Schema[] memory schemas // map to Seaport Improvement Proposal IDs
+        )
+    {
+        schemas = new Schema[](1);
+        schemas[0].id = 3003;
+        schemas[0].metadata = new bytes(0);
+
+        return ("MistyZone", schemas);
     }
 }
 
